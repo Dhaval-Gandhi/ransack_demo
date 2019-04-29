975ebc960e488eb4c0d5c7eb37a739342f59f3da
Dhaval Gandhi <dhaval.gandhi@botreetechnologies.com>
2019-04-29 11:48:41 +0530

    Remove add student link from list page Change database file name Define root Add few students in seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'students', to: 'students#index', as: 'students'
+  root to: 'students#index', as: 'students'
   get 'student/new', to: 'students#new', as: 'student_new'
   get 'search', to: 'student#search', as: 'student_search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts '###  STUDENT RECORD INSERTION...'
+students = [
+  {name: 'Jhon', city: 'New York', marks: 73},
+  {name: 'Arlen', city: 'Franklin', marks: 87},
+  {name: 'Barry', city: 'Georgetwon', marks: 94}
+]
+
+students.each do |student|
+  Student.find_or_create_by!(student)
+end
+puts '###  STUDENT RECORD INSERTED.'
